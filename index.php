fb031accc3b3b846eb978ddd14446876cdbc16c1
Andrei Pugatsov <andrei.pugatsov@itcollege.ee>
2017-04-03 16:06:22 +0300

    html alus tehtud, alustan php-ga

--- index.php
+++ index.php
@@ -6,7 +6,26 @@
 </head>
 
 <body>
+<p id="target-p">
+	
+</p>
 
+<p>
+	<form method="GET">
+		<p><input type = "text" name = "texttest"></p>
+		<p><input type = "color" name = "colortext" value="#ff0000"> &npsp; Text color</p>
+		<p><input type = "color" name = "colorbg" value="#00ff00"></p>
+		<p><input type = "color" name = "colorborder" value = "#0000ff"></p>
+		<p></p>
+		<p></p>
+		<p></p>
+		
+	</form>
+</p>
+
+<?php
+	
+?>
 
 </body>
 
